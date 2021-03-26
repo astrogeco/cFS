a1eafe9e007c012e74b3f2ad70cc9af4e564bd05
Gerardo E. Cruz-Ortiz <59618057+astrogeco@users.noreply.github.com>
2021-03-26 11:01:23 -0400

    Add CS app to targets.cmake

--- sample_defs/targets.cmake
+++ sample_defs/targets.cmake
@@ -89,7 +89,7 @@ SET(SPACECRAFT_ID 0x42)
 # and must be loaded explicitly via startup script or command.
 # This list is effectively appended to every TGTx_APPLIST in targets.cmake.  
 # Example:
-list(APPEND MISSION_GLOBAL_APPLIST sample_app sample_lib)
+list(APPEND MISSION_GLOBAL_APPLIST sample_app cs sample_lib)
 
 # The "MISSION_GLOBAL_STATIC_APPLIST" is similar to MISSION_GLOBAL_APPLIST
 # but the apps are statically linked.  
